1f88b00ee2e8ad57c5bcdc4cd2a36e89a1b8e539
david <davidpj@hotmail.co.uk>
2021-06-30 12:21:39 +0100

    configured managed identity

--- terraform-files/k8smodels/main.tf
+++ terraform-files/k8smodels/main.tf
@@ -1,10 +1,19 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 2.46.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
     }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  use_msi = true
+  subscription_id = "a9776c0d-e546-42a7-a002-63ba4a52ba62"
+  tenant_id = "de28047b-0625-4fad-adc8-487a110ddb70"
 }
 
 provider "azurerm" {
